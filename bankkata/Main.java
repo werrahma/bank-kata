ab5db115d57d4a15a96ea96dfd901e88243a177c
werrahma <errahmaniwalid15@gmail.com>
2025-01-30 11:11:48 +0100

    Added Main class to execute the banking application

---
+++ bankkata/Main.java
@@ -0,0 +1,21 @@
+package bankkata;
+
+import java.time.LocalDate;
+
+import skypay.Acount;
+
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Acount account = new Acount();
+        
+
+        account.deposit(1000, LocalDate.of(2012, 1, 10));
+        account.deposit(2000, LocalDate.of(2012, 1, 13));
+        account.withdraw(500, LocalDate.of(2012, 1, 14));
+
+        account.printStatement();
+    }
+}
